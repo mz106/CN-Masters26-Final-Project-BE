352dfb4076740a70ee0101b8b96b7ffcf612b54b
Michael Z <flashman123@fastmail.fm>
2021-09-06 16:33:32 +0100

    basic route added index.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 
-require("dotenv").config;
+require("dotenv").config();
 const express = require("express");
 
+const port = process.env.PORT;
+
+const app = express();
+
+app.use("/", (req, res) => {
+    res.status(200).send("Hello world");
+});
+
+app.listen(port, () => {
+    console.log("app is listening");
+});
 

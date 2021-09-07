3819018ced3ceb89211f22b5d8bf3f7c2ef7e382
Michael Z <flashman123@fastmail.fm>
2021-09-07 14:27:42 +0100

    error route added and working via insomia

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const connection = require("./db");
 const port = process.env.PORT;
 
 const indexRouter = require("./routes/index");
+const errorRouter = require("./routes/error");
 
 const app = express();
 
 app.use("/", indexRouter);
+app.use("*", errorRouter);
 
 app.listen(port, () => {
     console.log("app is listening");

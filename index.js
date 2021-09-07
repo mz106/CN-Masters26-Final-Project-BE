61da5eb982d654ddaa2f1d755e5ea680ecc0aa1d
Michael Z <flashman123@fastmail.fm>
2021-09-07 14:03:08 +0100

    basic products routes working insomia

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const connection = require("./db");
 const port = process.env.PORT;
 
 const indexRouter = require("./routes/index");
+const productRouter = require("./routes/products");
 
 const app = express();
 
+app.use("/products", productRouter);
 app.use("/", indexRouter);
+
 
 app.listen(port, () => {
     console.log("app is listening");

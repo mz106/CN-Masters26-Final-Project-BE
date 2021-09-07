433dfd441e5877e2f8c724822e885b8fd668cf1c
Michael Z <flashman123@fastmail.fm>
2021-09-07 10:45:17 +0100

    heroku connection added db.js

--- db.js
+++ db.js
@@ -1,17 +1,24 @@
 
 const { Sequelize } = require("sequelize");
 
-const connection = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-        // logging: false
-    }
-);
+let connection;
 
-console.log(connection)
+if (process.env.NODE_ENV === "production") {
+
+    connection = new Sequelize(process.env.DATABASE_URL);
+
+} else {
+    
+    connection = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST,
+            dialect: process.env.DB_DIALECT,
+            // logging: false
+        }
+    );
+}
 
 module.exports = connection;

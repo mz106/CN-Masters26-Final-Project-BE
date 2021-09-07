e021b3a41770b730221f632faaa66e5714ea26eb
Michael Z <flashman123@fastmail.fm>
2021-09-07 14:08:14 +0100

    utils.js and models/products added

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,15 @@
 
+const { response } = require("express");
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
     res.status(200).send("products/ working");
+});
+
+router.post("/weights", (req, res) => {
+    res.status(400).json({msg:"Here"});
 });
 
 router.get("/weights", (req, res) => {

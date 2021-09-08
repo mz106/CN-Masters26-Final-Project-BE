b391851ef877673cd91109785d4ca7bb7aec0f93
Michael Z <flashman123@fastmail.fm>
2021-09-08 10:28:19 +0100

    test routes db working

--- routes/test.js
+++ routes/test.js
@@ -6,9 +6,13 @@ const { addTest } = require("../utils/test");
 const router = express.Router();
 
 router.post("/addTest", async (req, res) => {
-    await addTest(req, res);
-    res.status(200).json({msg: `Test name ${req.body.name} has been created`});
-    
+
+    try {
+        await addTest(req, res);
+        res.status(200).json({msg: `Test name ${req.body.name} has been created`});
+    } catch (error) {
+        res.status(500).send(error)
+    }
 });
 
 module.exports = router;

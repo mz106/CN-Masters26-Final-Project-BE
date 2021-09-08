9e6b6c359d7a94605368250e32e9171c42877acf
Michael Z <flashman123@fastmail.fm>
2021-09-08 10:43:47 +0100

    test get route added

--- routes/test.js
+++ routes/test.js
@@ -5,20 +5,18 @@ const { addTest } = require("../utils/test");
 
 const router = express.Router();
 
-router.post("/addTest", async (req, res) => {
+router.get("/", async (req, res) => {
+    res.status(200).send("get test route works");
+});
 
+router.post("/", async (req, res) => {
 
     try {
         await addTest(req, res);
-        res.status(200).json({msg: `Test name ${req.body.name} has been created`});
+        res.status(201).json({msg: `Test name ${req.body.name} has been created`});
     } catch (error) {
         res.status(500).send(error)
     }
-
-    await addTest(req, res);
-    // res.status(200).json({msg: `Test name ${req.body.name} has been created`});
-    res.status(200).send("add test is working now");
-
 });
 
 module.exports = router;

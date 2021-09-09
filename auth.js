71d6c1e38c1c3b839a7298a5f730c5ed02e5c47b
Michael Z <flashman123@fastmail.fm>
2021-09-09 13:32:46 +0100

    auth

--- auth.js
+++ auth.js
@@ -1,29 +1,31 @@
 
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 
-// const User = require("./models/user");
+const User = require("./models/user");
 
-// //step one - 
+//step one - 
 
-// const register = async (name, email, password, done) => {
+const register = async (name, email, password, done) => {
+    console.log("hello world")
+    const saltRounds = 10;
+    console.log(saltRounds)
+    try {
+        if (!name || !email || !password) {
+            throw new Error("All fields are required");
+        }
 
-//     const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hash = await bcrypt.hash(password, salt);
+        const user = await User.build({name: name, email: email, passwordHash: hash});
 
-//     try {
-//         if (!name || !email || !password) {
-//             throw new Error("All fields are required");
-//         }
+        console.log(salt, hash, user)
+    } catch (error) {
+        done (error)
+    }
+};
 
-//         const salt = await bcrypt.genSalt(saltRounds);
-//         const hash = await bcrypt.hash(password, salt);
-//         const user = await User.build({name: name, email: email, passwordHash: hash});
+register()
 
-//         console.log(salt, hash, user)
-//     } catch (error) {
-//         done (error)
-//     }
-// };
-
-// module.exports = {
-//     register
-// };
+module.exports = {
+    register
+};
